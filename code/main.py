84af155189c484879c308b1e5d70e8545f9a4847
i-need-sleep <yh2689@nyu.edu>
2024-01-29 21:16:26 +0400

    uncomment

--- code/main.py
+++ code/main.py
@@ -61,7 +61,7 @@ def main(args):
         logger=logger,
         deterministic=True,
         num_sanity_val_steps=2,
-        # enable_progress_bar=args.debug,
+        enable_progress_bar=args.debug,
         log_every_n_steps=1,
         fast_dev_run=5 if args.debug else False,
         callbacks=[checkpoint_callback]
